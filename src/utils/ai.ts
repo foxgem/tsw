56e082d633aca7ce772022776375faf53b8b40f6
foxgem <jianhgreat@hotmail.com>
2024-09-21 11:16:33 +1000

    enhance summary prompt

--- src/utils/ai.ts
+++ src/utils/ai.ts
@@ -5,7 +5,8 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY!);
 
 const siEnglishTeacher =
   "你是一名资深英语老师有丰富的教学经验，可以深入浅出的用中文讲解英文疑难杂句和单词释义。";
-const siSummariser = "You are good at summarising articles.";
+const siSummariser =
+  "You are good at summarising articles. Based on a link passed by user, your task is to highlight the main key points in the article pointed by that link and output them in the format as users expect.";
 
 const generateTextAIFunction = async (prompt: string, systemInstruction: string) => {
   const model = genAI.getGenerativeModel({
@@ -30,11 +31,11 @@ export const explainWord = (word: string) =>
 
 export const summariseLink = (link: string) =>
   generateTextAIFunction(
-    `分析链接：${link}  中包含的文章内容并总结。要求如下：
-    1. 输出格式要求：采用与原文同样的语种进行输出。即若原文是英文，则输出用英文；原文是中文，输出用中文，以此类推。
-    2. 内容要求：
-    - 关键字。
-    - 用 100 字概括文章要点。
-    3. 一致性要求：将上面内容与原文重新对照进行修正`,
+    `分析链接： ${link} ，输出格式要求如下：
+    语言： 采用原文同语种。如：原文是英文，输出用英文；原文是中文，输出用中文，以此类推。
+    关键字： 5 个以内
+    概述：200 字以内，需要突出作者想要强调的要旨
+    段落大意：将文中每个段落用两三句话总结，并按原文一样的顺序排列输出。
+    最后进行一致性检查，确保整个输出不会出现前后矛盾与原文不符的地方，同时保证段落顺序的一致性。`,
     siSummariser
   );
